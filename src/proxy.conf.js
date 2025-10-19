c601643e08d9d251612b3ac782c46de85727e544
zaoldyck <a_bc@163.com>
2025-10-19 18:24:33 +0800

    1

--- src/proxy.conf.js
+++ src/proxy.conf.js
@@ -1,18 +1,24 @@
+// proxy.conf.js
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7277';
+const target =
+  env.ASPNETCORE_HTTPS_PORT
+    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+    : env.ASPNETCORE_URLS
+      ? env.ASPNETCORE_URLS.split(';')[0]
+      : 'https://localhost:7277';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
-      "^/api/",
-      "^/swagger"
+      '/weatherforecast', // 示例接口
+      '/api',             // 你自己的 API 前缀
+      '/swagger'          // Swagger UI/JSON
     ],
     target,
-    secure: false
+    secure: false,        // 本机自签证书一般需要 false
+    changeOrigin: true,   // 建议开启
   }
-]
+];
 
 module.exports = PROXY_CONFIG;
