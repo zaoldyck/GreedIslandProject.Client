5201ea25920fad233d974f7ad48c29bac255f3d1
Hong, Weicheng <Weicheng.Hong+carrier@carrier.com>
2025-10-21 16:24:37 +0800

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: '**', component: NotFound }
+];
--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, signal } from '@angular/core';
+import { LoginRequest } from '../../models/LoginRequest';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule ,MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule 
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  showPassword = signal(false);
 
+  form = this.fb.nonNullable.group({
+    account: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmit() {
+    debugger
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    const dto = this.form.getRawValue(); // { account, password }
+    // TODO: 在这里调用你的 Auth 服务 进行登录
+    console.log('login dto:', dto);
+  }
 }
