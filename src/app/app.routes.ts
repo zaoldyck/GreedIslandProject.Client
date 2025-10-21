944e04d3dd349020f04377958aa123143b1a4472
zaoldyck <a_bc@163.com>
2025-10-21 23:55:01 +0800

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { NotFound } from './features/not-found/not-found';
+import { Register } from './features/auth/pages/register/register';
+ 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'register', component: Register },
   { path: '**', component: NotFound }
 ];
--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -5,10 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   imports: [
-    ReactiveFormsModule ,MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule 
+    ReactiveFormsModule, MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
