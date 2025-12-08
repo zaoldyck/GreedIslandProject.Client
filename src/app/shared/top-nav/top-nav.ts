0fc264a57865cba8fe9728b778086cb0ceb4a7dc
Hong, Weicheng <Weicheng.Hong+carrier@carrier.com>
2025-12-08 16:21:36 +0800

    1

--- src/app/shared/top-nav/top-nav.ts
+++ src/app/shared/top-nav/top-nav.ts
@@ -1,15 +1,16 @@
-
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthenticationService } from '../../core/services/authentication-service';
 
 // Angular Material（standalone）
 import { MatIconModule } from '@angular/material/icon';
- 
-import {   MatButtonModule } from '@angular/material/button';
+
+import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-top-nav',
@@ -19,9 +20,10 @@ import { MatDividerModule } from '@angular/material/divider';
   imports: [
     // Router（若本组件里使用 <router-outlet> 就保留 RouterOutlet，否则可移除）
     RouterLinkActive, RouterLink, RouterOutlet,
- 
+    MatMenuModule,
     MatIconModule,
-    MatButtonModule,  
+    MatTooltipModule,
+    MatButtonModule,
     MatSidenavModule,                // ★ 必须：提供 <mat-sidenav-container>/<mat-sidenav>/<mat-sidenav-content>
     MatListModule,                   // 菜单列表（<mat-nav-list>/<a mat-list-item>）
     MatDividerModule,
@@ -35,5 +37,10 @@ export class TopNav {
     this.authenticationService.logout().subscribe({
       next: () => this.router.navigateByUrl('/'),
     });
+  }
+
+  getInitials(name?: string | null): string {
+    if (!name) return '?';
+    return name.slice(0, 2).toUpperCase();
   }
 }
